bea2767938aef3e59ea59682a709f9fbf5260d44
Max van gen Hassend <MaxvangenHasssend@googlemail.com>
2017-12-26 01:16:31 -0800

    formatting

--- handleFlags.js
+++ handleFlags.js
@@ -1,102 +1,103 @@
-let Utils = require('utils')
+let Utils = require('utils');
 
 exports.handleflags = function () {
-  let removeflags = (Game.cpu.bucket < 300)
+  let removeflags = (Game.cpu.bucket < 300);
 
   for (let flagN in Game.flags) {
-    let flag = Game.flags[flagN]
+    let flag = Game.flags[flagN];
 
     if (flag.memory.remove) {
-      flag.remove()
-      continue
+      flag.remove();
+      continue;
     }
+
+    // TODO: refactor to switch case
 
     if (flag.color === COLOR_WHITE) {
       if (removeflags) {
-        flag.remove()
+        flag.remove();
       }
 
       if (Game.time % 200 === 0) {
-        let terrain = Game.map.getTerrainAt(flag.pos)
-        let visited = flag.memory.visited ? flag.memory.visited : 1
-        let timeout = flag.memory.timeout ? flag.memory.timeout : 0
-        let diff = visited - timeout
+        let terrain = Game.map.getTerrainAt(flag.pos);
+        let visited = flag.memory.visited ? flag.memory.visited : 1;
+        let timeout = flag.memory.timeout ? flag.memory.timeout : 0;
+        let diff = visited - timeout;
         if (diff >= 1) {
-          flag.memory.timeout = (terrain === 'swamp') ? timeout + 4 : timeout + 1
+          flag.memory.timeout = (terrain === 'swamp') ? timeout + 4 : timeout + 1;
           // console.log('reducing visited count');
         } else {
           // console.log('flag was not visited enough -> remove flag');
-          flag.memory.remove = true
-          flag.remove()
+          flag.memory.remove = true;
+          flag.remove();
         }
       }
     } else if (flag.color === COLOR_YELLOW) {
-      Memory.gatheringpoint = flag.pos
-      flag.memory.remove = true
-      flag.remove()
+      Memory.gatheringpoint = flag.pos;
+      flag.memory.remove = true;
+      flag.remove();
     } else if (flag.color === COLOR_GREEN) {
-      warroom = flag.pos.roomName
-      Memory.warroom = flag.pos.roomName
-      flag.remove()
+      /* warroom = flag.pos.roomName;
+      Memory.warroom = flag.pos.roomName;
+      flag.remove(); */
     } else if (flag.color === COLOR_BROWN && !flag.memory.remove) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos)
+      var sourceroomname = Utils.findClosestRoom(flag.pos);
       // console.log(sourceroomname);
       if (sourceroomname) {
-        let slaverooms = Game.rooms[sourceroomname].memory.slaverooms || []
-        slaverooms.push(flag.pos)
-        Game.rooms[sourceroomname].memory.slaverooms = slaverooms
-        flag.memory.remove = true
-        flag.remove()
+        let slaverooms = Game.rooms[sourceroomname].memory.slaverooms || [];
+        slaverooms.push(flag.pos);
+        Game.rooms[sourceroomname].memory.slaverooms = slaverooms;
+        flag.memory.remove = true;
+        flag.remove();
       }
-    } else if (flag.color === COLOR_BLUE && !flag.memory.remove) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos)
-      console.log('found claimroom: ' + sourceroomname)
-      Game.rooms[sourceroomname].memory.claimroom = flag.pos.roomName
-      flag.memory.remove = true
-      flag.remove()
-    } else if (flag.color === COLOR_GREY) {
+    } else if (flag.color === COLOR_BLUE) {
+      var sourceroomname = Utils.findClosestRoom(flag.pos);
+      console.log('found claimroom: ' + sourceroomname);
+      Game.rooms[sourceroomname].memory.claimroom = flag.pos.roomName;
+      flag.memory.remove = true;
+      flag.remove();
+    } else if (flag.color === COLOR_GREY) { // LOOT
       if (flag.secondaryColor === COLOR_RED) {
         // console.log('flag detected');
 
-        var sourceroomname = Utils.findClosestRoom(flag.pos)
-        Game.rooms[sourceroomname].memory.lootroom = flag.pos.roomName
-        flag.memory.remove = true
-        flag.remove()
+        var sourceroomname = Utils.findClosestRoom(flag.pos);
+        Game.rooms[sourceroomname].memory.lootroom = flag.pos.roomName;
+        flag.memory.remove = true;
+        flag.remove();
       } else if (flag.secondaryColor === COLOR_BLUE) {
-        flag.room.memory.upgradepos = flag.pos
-        flag.remove()
+        flag.room.memory.upgradepos = flag.pos;
+        flag.remove();
       }
     } else if (flag.color === COLOR_ORANGE) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos)
-      Game.rooms[sourceroomname].memory.attackinprogress = true
-      Game.rooms[sourceroomname].memory.warroom = flag.pos.roomName
-      flag.remove()
+      var sourceroomname = Utils.findClosestRoom(flag.pos);
+      Game.rooms[sourceroomname].memory.attackinprogress = true;
+      Game.rooms[sourceroomname].memory.warroom = flag.pos.roomName;
+      flag.remove();
     } else if (flag.color === COLOR_RED) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos)
-      Game.rooms[sourceroomname].memory.guidedattack = true
+      var sourceroomname = Utils.findClosestRoom(flag.pos);
+      Game.rooms[sourceroomname].memory.guidedattack = true;
       // Memory.attackinprogress = true;
-      flag.remove()
-      Memory.gatheringpoint = null
-    } else if (flag.color === COLOR_CYAN && !flag.memory.remove) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos)
+      flag.remove();
+      Memory.gatheringpoint = null;
+    } else if (flag.color === COLOR_CYAN) {
+      var sourceroomname = Utils.findClosestRoom(flag.pos);
 
-      let cords = flag.pos.roomName.substr(1).replace('N', ',').replace('S', ',').split(',')
+      let cords = flag.pos.roomName.substr(1).replace('N', ',').replace('S', ',').split(',');
       if (Number(cords[0]) % 10 === 5 && Number(cords[1]) % 10 === 5) {
-        centralroom = true
-        console.log('add central room to list')
-        Game.rooms[sourceroomname].memory.centralroom = flag.pos.roomName
+        centralroom = true;
+        console.log('add central room to list');
+        Game.rooms[sourceroomname].memory.centralroom = flag.pos.roomName;
       } else {
-        let keeperrooms = Game.rooms[sourceroomname].memory.keeperrooms || []
-        keeperrooms.push(flag.pos.roomName)
-        Game.rooms[sourceroomname].memory.keeperrooms = keeperrooms
+        let keeperrooms = Game.rooms[sourceroomname].memory.keeperrooms || [];
+        keeperrooms.push(flag.pos.roomName);
+        Game.rooms[sourceroomname].memory.keeperrooms = keeperrooms;
       }
 
-      // console.log('keeperrooms: ' + JSON.stringify8keeperrooms);
-      flag.memory.remove = true
-      flag.remove()
+      flag.memory.remove = true;
+      flag.remove();
     } else if (flag.color === COLOR_PURPLE) {
       // flag.remove();
       // TODO: remove if no building
     }
   }
-}
+};
--- role.keeper.js
+++ role.keeper.js
@@ -2,83 +2,83 @@ let roleKeeper = {
 
   act: function (creep) {
     if (creep.memory.ranged) {
-      return this.ranged_act(creep)
+      return this.ranged_act(creep);
     }
 
     // adjacent creeps
     var targets = creep.room.find(FIND_HOSTILE_CREEPS).filter(function (hc) {
-      return (creep.pos.inRangeTo(hc, 1))
-    })
+      return (creep.pos.inRangeTo(hc, 1));
+    });
 
     if (targets.length > 0) {
-      var target = creep.pos.findClosestByRange(targets)
+      var target = creep.pos.findClosestByRange(targets);
       if (creep.attack(target) !== OK) {
         if (creep.moveTo(target, {reusePath: 0}) === OK) {
-          creep.attack(target)
+          creep.attack(target);
         }
-        this.afteract(creep)
+        this.afteract(creep);
       } else if (target.getActiveBodyparts(ATTACK) > 0) {
-        this.afteract(creep)
+        this.afteract(creep);
       }
-      return true
+      return true;
     }
 
     // offensive creeps but no keepers
-    var targets = creep.room.find(FIND_HOSTILE_CREEPS)
+    var targets = creep.room.find(FIND_HOSTILE_CREEPS);
     if (creep.memory.ignoreneutrals) {
       targets = targets.filter(function (hc) {
-        return (hc.owner.username !== 'Source Keeper' || creep.pos.inRangeTo(hc, 4)) && (hc.getActiveBodyparts(ATTACK) > 0 || hc.getActiveBodyparts(RANGED_ATTACK > 0) || hc.getActiveBodyparts(HEAL) > 0)
-      })
+        return (hc.owner.username !== 'Source Keeper' || creep.pos.inRangeTo(hc, 4)) && (hc.getActiveBodyparts(ATTACK) > 0 || hc.getActiveBodyparts(RANGED_ATTACK > 0) || hc.getActiveBodyparts(HEAL) > 0);
+      });
     }
 
     if (targets.length > 0) {
-      var target = creep.pos.findClosestByRange(targets)
+      var target = creep.pos.findClosestByRange(targets);
 
       if (creep.attack(target) !== OK) {
         if (creep.goTo(target.pos, 2, 10) === OK) {
-          creep.attack(target)
+          creep.attack(target);
         }
-        this.afteract(creep)
+        this.afteract(creep);
       }
 
-      return true
+      return true;
     }
 
     // offensive creeps
     var targets = creep.room.find(FIND_HOSTILE_CREEPS).filter(function (hc) {
-      return (hc.getActiveBodyparts(ATTACK) > 0 || hc.getActiveBodyparts(RANGED_ATTACK) > 0 || hc.getActiveBodyparts(HEAL) > 0)
-    })
+      return (hc.getActiveBodyparts(ATTACK) > 0 || hc.getActiveBodyparts(RANGED_ATTACK) > 0 || hc.getActiveBodyparts(HEAL) > 0);
+    });
 
     if (targets.length > 0) {
-      var target = creep.pos.findClosestByRange(targets)
+      var target = creep.pos.findClosestByRange(targets);
 
       if (creep.attack(target) !== OK) {
         if (creep.goTo(target.pos, 2, 10) === OK) {
-          creep.attack(target)
+          creep.attack(target);
         }
         /* if (creep.moveTo(target) === OK){
                     creep.attack(target);
                 } */
-        this.afteract(creep)
+        this.afteract(creep);
       }
 
-      return true
+      return true;
     }
 
     // normal creeps
-    var targets = creep.room.find(FIND_HOSTILE_CREEPS)
+    var targets = creep.room.find(FIND_HOSTILE_CREEPS);
 
     if (targets.length > 0) {
-      var target = creep.pos.findClosestByRange(targets)
+      var target = creep.pos.findClosestByRange(targets);
       if (creep.attack(target) !== OK) {
         // console.log('blaaa');
         if (creep.moveTo(target, {reusePath: 0}) === OK) {
-          creep.attack(target)
+          creep.attack(target);
         }
-        creep.memory.lasttarget = target.pos
-        this.afteract(creep)
+        creep.memory.lasttarget = target.pos;
+        this.afteract(creep);
       }
-      return true
+      return true;
     }
 
     // damaged
@@ -87,19 +87,19 @@ let roleKeeper = {
 
       if (creep.memory.home && creep.memory.home !== creep.room.name) {
         // console.log('creep is outside -> go home');
-        let homeroom = Game.rooms[creep.memory.home]
-        let exitDir = creep.room.findExitTo(homeroom)
-        let exit = creep.pos.findClosestByRange(exitDir)
-        creep.moveTo(exit)
-        return
+        let homeroom = Game.rooms[creep.memory.home];
+        let exitDir = creep.room.findExitTo(homeroom);
+        let exit = creep.pos.findClosestByRange(exitDir);
+        creep.moveTo(exit);
+        return;
       } else {
         // console.log('creep is at home');
-        creep.moveTo(creep.room.controller)
+        creep.moveTo(creep.room.controller);
       }
-      return true
+      return true;
     }
 
-    return false
+    return false;
   },
 
   ranged_act: function (creep) {
@@ -110,54 +110,54 @@ let roleKeeper = {
       if (creep.room.name === creep.memory.home) {
         // console.log('creep is in homeroom');
         // set roomcontroller as default safepoint
-        creep.memory.safepoint = creep.room.controller.pos
+        creep.memory.safepoint = creep.room.controller.pos;
       } else {
         // console.log('creep is in other room');
-        creep.memory.safepoint = creep.pos
+        creep.memory.safepoint = creep.pos;
       }
     }
 
-    var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1)
+    var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1);
     if (targets.length > 0) {
-      creep.rangedMassAttack()
-      var roompos = new RoomPosition(creep.memory.safepoint.x, creep.memory.safepoint.y, creep.room.name)
-      creep.moveTo(roompos, {reusePath: 0})
-      return true
+      creep.rangedMassAttack();
+      var roompos = new RoomPosition(creep.memory.safepoint.x, creep.memory.safepoint.y, creep.room.name);
+      creep.moveTo(roompos, {reusePath: 0});
+      return true;
     }
 
-    var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 2)
+    var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 2);
     if (targets.length > 0) {
       // console.log('hostile creeps in range 2 -> run')
-      creep.rangedAttack(targets[0])
-      var roompos = new RoomPosition(creep.memory.safepoint.x, creep.memory.safepoint.y, creep.room.name)
-      creep.moveTo(roompos, {reusePath: 0})
-      return true
+      creep.rangedAttack(targets[0]);
+      var roompos = new RoomPosition(creep.memory.safepoint.x, creep.memory.safepoint.y, creep.room.name);
+      creep.moveTo(roompos, {reusePath: 0});
+      return true;
     }
 
-    var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3)
+    var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
     if (targets.length > 0) {
       // console.log('hostile creeps in range 3 -> do nothing')
-      creep.rangedAttack(targets[0])
-      return true
+      creep.rangedAttack(targets[0]);
+      return true;
     }
 
     // offensive creeps
-    var targets = creep.room.find(FIND_HOSTILE_CREEPS)
+    var targets = creep.room.find(FIND_HOSTILE_CREEPS);
     if (creep.memory.ignoreneutrals) {
       targets = targets.filter(function (hc) {
-        return (hc.owner.username !== 'Source Keeper' || creep.pos.inRangeTo(hc, 4))
-      })
+        return (hc.owner.username !== 'Source Keeper' || creep.pos.inRangeTo(hc, 4));
+      });
     }
 
     // move to enemy
     if (targets.length > 0) {
-      console.log('targets detected')
+      console.log('targets detected');
 
-      let target = creep.pos.findClosestByRange(targets)
+      let target = creep.pos.findClosestByRange(targets);
       if (creep.moveTo(target, {reusePath: 0}) === OK) {
-        creep.rangedAttack(target)
+        creep.rangedAttack(target);
       }
-      return true
+      return true;
     }
     // damaged
 
@@ -166,79 +166,79 @@ let roleKeeper = {
 
       if (creep.memory.home && creep.memory.home !== creep.room.name) {
         // console.log('creep is outside -> go home');
-        let homeroom = Game.rooms[creep.memory.home]
-        let exitDir = creep.room.findExitTo(homeroom)
-        let exit = creep.pos.findClosestByRange(exitDir)
-        creep.moveTo(exit)
-        return
+        let homeroom = Game.rooms[creep.memory.home];
+        let exitDir = creep.room.findExitTo(homeroom);
+        let exit = creep.pos.findClosestByRange(exitDir);
+        creep.moveTo(exit);
+        return;
       } else {
         // console.log('creep is at home');
-        creep.moveTo(creep.room.controller)
+        creep.moveTo(creep.room.controller);
       }
-      return true
+      return true;
     }
 
-    return false
+    return false;
   },
 
   init: function (creep) {
-    creep.memory.init = true
-    let rangedparts = creep.getActiveBodyparts(RANGED_ATTACK)
+    creep.memory.init = true;
+    let rangedparts = creep.getActiveBodyparts(RANGED_ATTACK);
     if (rangedparts > 0) {
-      creep.memory.ranged = true
+      creep.memory.ranged = true;
     }
     if (creep.getActiveBodyparts(HEAL) > 0) {
-      creep.memory.hasheal = true
+      creep.memory.hasheal = true;
     }
   },
 
   afteract: function (creep) {
     if (creep.memory.hasheal && creep.hits < creep.hitsMax) {
-      creep.heal(creep)
+      creep.heal(creep);
     }
   },
 
   /** @param {Creep} creep **/
   run: function (creep) {
     if (!creep.memory.init) {
-      this.init(creep)
+      this.init(creep);
     }
 
     // this.afteract(creep);
 
-    let actiondone = this.act(creep)
+    let actiondone = this.act(creep);
 
     if (actiondone) {
-      return
+      return;
     }
 
-    this.afteract(creep)
+    this.afteract(creep);
 
     if (creep.room.name !== creep.memory.room) {
-      let roompos = new RoomPosition(25, 25, creep.memory.room)
+      let roompos = new RoomPosition(25, 25, creep.memory.room);
 
       if (creep.memory.lasttarget && creep.memory.lasttarget.roomName === creep.memory.room) {
-        roompos = new RoomPosition(creep.memory.lasttarget.x, creep.memory.lasttarget.y, creep.memory.lasttarget.roomName)
+        roompos = new RoomPosition(creep.memory.lasttarget.x, creep.memory.lasttarget.y, creep.memory.lasttarget.roomName);
       }
 
-      creep.goTo(roompos)
-      return
+      creep.goTo(roompos);
+      return;
     }
 
-    let lowestrespawntime = 1000
-    let nextrespawn = null
+    let lowestrespawntime = 1000;
+    let nextrespawn = null;
     let lairs = creep.room.find(FIND_HOSTILE_STRUCTURES).filter(function (structure) {
-      return structure.structureType === STRUCTURE_KEEPER_LAIR
+      return structure.structureType === STRUCTURE_KEEPER_LAIR;
     }).forEach(function (lair) {
       if (lair.ticksToSpawn < lowestrespawntime) {
-        lowestrespawntime = lair.ticksToSpawn
-        nextrespawn = lair
+        lowestrespawntime = lair.ticksToSpawn;
+        nextrespawn = lair;
       }
-    })
+    });
     if (!creep.pos.inRangeTo(nextrespawn)) {
-      creep.goTo(nextrespawn.pos)
+      creep.goTo(nextrespawn.pos);
     }
   }
-}
+};
 
-module.exports = roleKeeper
+module.exports = roleKeeper;
