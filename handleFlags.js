160d6ffd45b0224233cfe6205ea272e1f81abe5b
Max van gen Hassend <MaxvangenHasssend@googlemail.com>
2017-12-26 01:42:32 -0800

    handleFlags use switch case

--- handleFlags.js
+++ handleFlags.js
@@ -1,8 +1,6 @@
 let Utils = require('utils');
 
 exports.handleflags = function () {
-  let removeflags = (Game.cpu.bucket < 300);
-
   for (let flagN in Game.flags) {
     let flag = Game.flags[flagN];
 
@@ -11,93 +9,89 @@ exports.handleflags = function () {
       continue;
     }
 
-    // TODO: refactor to switch case
-
-    if (flag.color === COLOR_WHITE) {
-      if (removeflags) {
+    let sourceRoomName;
+    switch (flag.color) {
+      case COLOR_WHITE:
+        if (Game.time % 200 === 0) {
+          let terrain = Game.map.getTerrainAt(flag.pos);
+          let visited = flag.memory.visited ? flag.memory.visited : 1;
+          let timeout = flag.memory.timeout ? flag.memory.timeout : 0;
+          let diff = visited - timeout;
+          if (diff >= 1) {
+            flag.memory.timeout = (terrain === 'swamp') ? timeout + 4 : timeout + 1;
+            // console.log('reducing visited count');
+          } else {
+            // console.log('flag was not visited enough -> remove flag');
+            flag.memory.remove = true;
+            flag.remove();
+          }
+        }
+        break;
+      case COLOR_YELLOW:
+        Memory.gatheringpoint = flag.pos;
+        flag.memory.remove = true;
         flag.remove();
-      }
-
-      if (Game.time % 200 === 0) {
-        let terrain = Game.map.getTerrainAt(flag.pos);
-        let visited = flag.memory.visited ? flag.memory.visited : 1;
-        let timeout = flag.memory.timeout ? flag.memory.timeout : 0;
-        let diff = visited - timeout;
-        if (diff >= 1) {
-          flag.memory.timeout = (terrain === 'swamp') ? timeout + 4 : timeout + 1;
-          // console.log('reducing visited count');
-        } else {
-          // console.log('flag was not visited enough -> remove flag');
+        break;
+      case COLOR_BROWN:
+        sourceRoomName = Utils.findClosestRoom(flag.pos);
+        if (sourceRoomName) {
+          let slaverooms = Game.rooms[sourceRoomName].memory.slaverooms || [];
+          slaverooms.push(flag.pos);
+          Game.rooms[sourceRoomName].memory.slaverooms = slaverooms;
           flag.memory.remove = true;
           flag.remove();
         }
-      }
-    } else if (flag.color === COLOR_YELLOW) {
-      Memory.gatheringpoint = flag.pos;
-      flag.memory.remove = true;
-      flag.remove();
-    } else if (flag.color === COLOR_GREEN) {
-      /* warroom = flag.pos.roomName;
-      Memory.warroom = flag.pos.roomName;
-      flag.remove(); */
-    } else if (flag.color === COLOR_BROWN && !flag.memory.remove) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos);
-      // console.log(sourceroomname);
-      if (sourceroomname) {
-        let slaverooms = Game.rooms[sourceroomname].memory.slaverooms || [];
-        slaverooms.push(flag.pos);
-        Game.rooms[sourceroomname].memory.slaverooms = slaverooms;
+        break;
+      case COLOR_BLUE:
+        sourceRoomName = Utils.findClosestRoom(flag.pos);
+        console.log('found claimroom: ' + sourceRoomName);
+        Game.rooms[sourceRoomName].memory.claimroom = flag.pos.roomName;
         flag.memory.remove = true;
         flag.remove();
-      }
-    } else if (flag.color === COLOR_BLUE) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos);
-      console.log('found claimroom: ' + sourceroomname);
-      Game.rooms[sourceroomname].memory.claimroom = flag.pos.roomName;
-      flag.memory.remove = true;
-      flag.remove();
-    } else if (flag.color === COLOR_GREY) { // LOOT
-      if (flag.secondaryColor === COLOR_RED) {
-        // console.log('flag detected');
+        break;
+      case COLOR_GREY:
+        if (flag.secondaryColor === COLOR_RED) {
+          // console.log('flag detected');
 
-        var sourceroomname = Utils.findClosestRoom(flag.pos);
-        Game.rooms[sourceroomname].memory.lootroom = flag.pos.roomName;
+          sourceRoomName = Utils.findClosestRoom(flag.pos);
+          Game.rooms[sourceRoomName].memory.lootroom = flag.pos.roomName;
+          flag.memory.remove = true;
+          flag.remove();
+        } else if (flag.secondaryColor === COLOR_BLUE) {
+          flag.room.memory.upgradepos = flag.pos;
+          flag.remove();
+        }
+        break;
+      case COLOR_ORANGE:
+        sourceRoomName = Utils.findClosestRoom(flag.pos);
+        Game.rooms[sourceRoomName].memory.attackinprogress = true;
+        Game.rooms[sourceRoomName].memory.warroom = flag.pos.roomName;
+        flag.remove();
+        break;
+      case COLOR_RED:
+        sourceRoomName = Utils.findClosestRoom(flag.pos);
+        Game.rooms[sourceRoomName].memory.guidedattack = true;
+        flag.remove();
+        Memory.gatheringpoint = null;
+        break;
+      case COLOR_CYAN:
+        sourceRoomName = Utils.findClosestRoom(flag.pos);
+
+        let cords = flag.pos.roomName.substr(1).replace('N', ',').replace('S', ',').split(',');
+        if (Number(cords[0]) % 10 === 5 && Number(cords[1]) % 10 === 5) {
+          console.log('add central room to list');
+          Game.rooms[sourceRoomName].memory.centralroom = flag.pos.roomName;
+        } else {
+          let keeperrooms = Game.rooms[sourceRoomName].memory.keeperrooms || [];
+          keeperrooms.push(flag.pos.roomName);
+          Game.rooms[sourceRoomName].memory.keeperrooms = keeperrooms;
+        }
+
         flag.memory.remove = true;
         flag.remove();
-      } else if (flag.secondaryColor === COLOR_BLUE) {
-        flag.room.memory.upgradepos = flag.pos;
-        flag.remove();
-      }
-    } else if (flag.color === COLOR_ORANGE) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos);
-      Game.rooms[sourceroomname].memory.attackinprogress = true;
-      Game.rooms[sourceroomname].memory.warroom = flag.pos.roomName;
-      flag.remove();
-    } else if (flag.color === COLOR_RED) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos);
-      Game.rooms[sourceroomname].memory.guidedattack = true;
-      // Memory.attackinprogress = true;
-      flag.remove();
-      Memory.gatheringpoint = null;
-    } else if (flag.color === COLOR_CYAN) {
-      var sourceroomname = Utils.findClosestRoom(flag.pos);
-
-      let cords = flag.pos.roomName.substr(1).replace('N', ',').replace('S', ',').split(',');
-      if (Number(cords[0]) % 10 === 5 && Number(cords[1]) % 10 === 5) {
-        centralroom = true;
-        console.log('add central room to list');
-        Game.rooms[sourceroomname].memory.centralroom = flag.pos.roomName;
-      } else {
-        let keeperrooms = Game.rooms[sourceroomname].memory.keeperrooms || [];
-        keeperrooms.push(flag.pos.roomName);
-        Game.rooms[sourceroomname].memory.keeperrooms = keeperrooms;
-      }
-
-      flag.memory.remove = true;
-      flag.remove();
-    } else if (flag.color === COLOR_PURPLE) {
-      // flag.remove();
-      // TODO: remove if no building
+        break;
+      default:
+        console.log('flag.color ' + flag.color + ' has no implementation yet');
     }
   }
 };
