eb7cd295b1a21a5322b0b659b6c318133aeae9dc
Max van gen Hassend <MaxvangenHasssend@googlemail.com>
2017-12-29 01:07:22 -0800

    creep.spawning in creepStatus

--- CreepStatus.js
+++ CreepStatus.js
@@ -1,31 +1,31 @@
 let roles = ['builder', 'upgrader', 'outsider', 'sltrans', 'harvester', 'transporter', 'reserver', 'defender',
-  'attacker', 'healer', 'specialbuilder', 'mineral', 'mineraltransporter', 'dismantler', 'keeper', 'gatherer', 'Specialdefender', 'looter', 'claim' ]
+  'attacker', 'healer', 'specialbuilder', 'mineral', 'mineraltransporter', 'dismantler', 'keeper', 'gatherer', 'Specialdefender', 'looter', 'claim' ];
 
 function addRolesToRoom (room) {
-  room.myCreeps = {}
-  roles.forEach(key => { room.myCreeps[key] = [] })
+  room.myCreeps = {};
+  roles.forEach(key => { room.myCreeps[key] = []; });
 }
 
 exports.AddCreepStatusToRoomInfo = function () {
   for (let key in Game.rooms) {
     if (Game.rooms.hasOwnProperty(key)) {
-      let room = Game.rooms[key]
+      let room = Game.rooms[key];
       if (room.controller && room.controller.my) {
         // I can only have creeps I own myself;
-        addRolesToRoom(room)
+        addRolesToRoom(room);
       }
     }
   }
 
   for (let key in Game.creeps) {
     if (Game.creeps.hasOwnProperty(key)) {
-      let creep = Game.creeps[key]
-      if (creep.ticksToLive > 100) {
-        let home = creep.memory.home
-        let room = Game.rooms[home]
-        room.myCreeps[creep.memory.role].push(creep) // TODO add array in other for loop
+      let creep = Game.creeps[key];
+      if (creep.ticksToLive > 100 || creep.spawning) {
+        let home = creep.memory.home;
+        let room = Game.rooms[home];
+        room.myCreeps[creep.memory.role].push(creep); // TODO add array in other for loop
         // console.log(JSON.stringify(room.myCreeps));
       }
     }
   }
-}
+};
